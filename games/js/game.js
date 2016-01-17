8a8bbd297df20b21b124c8b225ee0a8ad4e60715
yunleiz <zhyunlei@hotmail.com>
2016-01-16 21:18:47 -0500

    refactored

--- games/js/game.js
+++ games/js/game.js
@@ -1,56 +1,51 @@
+var then;
 // Create the canvas
-var canvas = document.createElement("canvas");
-var ctx = canvas.getContext("2d");
-canvas.width = 512;
-canvas.height = 480;
-document.body.appendChild(canvas);
+var canvas;
+var ctx;
 
 // Background image
-var bgReady = false;
-var bgImage = new Image();
-bgImage.onload = function () {
+var bgReady;
+var bgImage;
+
+/*bgImage.onload = function () {
   bgReady = true;
-};
-bgImage.src = "images/background.png";
+};*/
+
 
 // Hero image
-var heroReady = false;
-var heroImage = new Image();
-heroImage.onload = function () {
+var heroReady;
+var heroImage;
+/*heroImage.onload = function () {
   heroReady = true;
-};
-heroImage.src = "images/hero.png";
+};*/
 
 // Monster image
-var monsterReady = false;
-var monsterImage = new Image();
-monsterImage.onload = function () {
+var monsterReady;
+var monsterImage
+/*monsterImage.onload = function () {
   monsterReady = true;
-};
-monsterImage.src = "images/monster.png";
+};*/
 
 // Game objects
-var hero = {
-  speed: 256 // movement in pixels per second
-};
-var monster = {};
-var monstersCaught = 0;
+var hero;
+var monster;
+var monstersCaught;
 
 // Handle keyboard controls
-var keysDown = {};
+var keysDown;
 
-addEventListener("keydown", function (e) {
+window.addEventListener("keydown", function (e) {
   keysDown[e.keyCode] = true;
 }, false);
 
-addEventListener("keyup", function (e) {
+window.addEventListener("keyup", function (e) {
   delete keysDown[e.keyCode];
 }, false);
 
 // Reset the game when the player catches a monster
 var reset = function () {
-  hero.x = canvas.width / 2;
-  hero.y = canvas.height / 2;
+  hero.x = canvas.offsetWidth / 2;
+  hero.y = canvas.offsetHeight / 2;
 
   // Throw the monster somewhere on the screen randomly
   monster.x = 32 + (Math.random() * (canvas.width - 64));
@@ -108,6 +103,7 @@ var render = function () {
 
 // The main game loop
 var main = function () {
+  
   var now = Date.now();
   var delta = now - then;
 
@@ -116,15 +112,60 @@ var main = function () {
 
   then = now;
 
+  // Cross-browser support for requestAnimationFrame
+  var w = window;
+  requestAnimationFrame = w.requestAnimationFrame 
+                        || w.webkitRequestAnimationFrame 
+                        || w.msRequestAnimationFrame 
+                        || w.mozRequestAnimationFrame;
   // Request to do this again ASAP
   requestAnimationFrame(main);
 };
 
-// Cross-browser support for requestAnimationFrame
-var w = window;
-requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
+var init = function(){
+  then = Date.now();
+  canvas = document.getElementById("game");
+  ctx = canvas.getContext("2d");
+  canvas.width = 512;
+  canvas.height = 480;
+
+  // Background image
+  bgReady = false;
+  bgImage = new Image();
+  bgImage.onload = function () {
+      bgReady = true;
+  };
+  bgImage.src = "images/background.png";
+  // Hero image
+  heroReady = false;
+  heroImage = new Image();
+  heroImage.onload = function () {
+      heroReady = true;
+  };
+  heroImage.src = "images/hero.png";
+  // Monster image
+  monsterReady = false;
+  monsterImage = new Image();
+  monsterImage.onload = function () {
+      monsterReady = true;
+  };
+  monsterImage.src = "images/monster.png";
+
+  hero = {
+  speed: 256 // movement in pixels per second
+  };
+
+  monster = {};
+  monstersCaught = 0;
+
+  // Handle keyboard controls
+  keysDown = {};
+};
+
 
 // Let's play this game!
-var then = Date.now();
-reset();
-main();
+var onload = function(){
+  init();
+  reset();
+  main();
+};
